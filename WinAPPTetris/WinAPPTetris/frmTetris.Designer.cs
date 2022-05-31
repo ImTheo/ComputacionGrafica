b4d9b9663be9031a359adcdd2e28d86f2113f02e
imTheo <theomrosero@gmail.com>
2022-05-31 08:38:11 -0500

    TetrisApp

---
+++ WinAPPTetris/WinAPPTetris/frmTetris.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace WinAPPTetris
+{
+    partial class frmTetris
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.picClose = new System.Windows.Forms.PictureBox();
+            this.picPause = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPause)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Location = new System.Drawing.Point(11, 4);
+            this.picCanvas.Margin = new System.Windows.Forms.Padding(2);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(469, 435);
+            this.picCanvas.TabIndex = 1;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Click += new System.EventHandler(this.picCanvas_Click);
+            // 
+            // picClose
+            // 
+            this.picClose.Location = new System.Drawing.Point(281, 343);
+            this.picClose.Margin = new System.Windows.Forms.Padding(2);
+            this.picClose.Name = "picClose";
+            this.picClose.Size = new System.Drawing.Size(113, 49);
+            this.picClose.TabIndex = 3;
+            this.picClose.TabStop = false;
+            this.picClose.Click += new System.EventHandler(this.picClose_Click);
+            // 
+            // picPause
+            // 
+            this.picPause.Location = new System.Drawing.Point(60, 95);
+            this.picPause.Margin = new System.Windows.Forms.Padding(2);
+            this.picPause.Name = "picPause";
+            this.picPause.Size = new System.Drawing.Size(164, 256);
+            this.picPause.TabIndex = 2;
+            this.picPause.TabStop = false;
+            this.picPause.Click += new System.EventHandler(this.picPause_Click);
+            this.picPause.MouseEnter += new System.EventHandler(this.picPause_MouseEnter);
+            this.picPause.MouseLeave += new System.EventHandler(this.picPause_MouseLeave);
+            // 
+            // frmTetris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 450);
+            this.Controls.Add(this.picPause);
+            this.Controls.Add(this.picClose);
+            this.Controls.Add(this.picCanvas);
+            this.Name = "frmTetris";
+            this.Text = "Tetris";
+            this.Load += new System.EventHandler(this.frmTetris_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPause)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.PictureBox picClose;
+        private System.Windows.Forms.PictureBox picPause;
+    }
+}
+
---
+++ WinAPPTetris/WinAPPTetris/frmTetris.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAPPTetris
+{
+    public partial class frmTetris : Form
+    {
+        CTetris tetris = new CTetris();
+        public frmTetris()
+        {
+            InitializeComponent();
+        }
+
+        private void picCanvas_Click(object sender, EventArgs e)
+        {
+            tetris.GraphExit(picClose);
+            tetris.GraphMainArea(picCanvas);
+            tetris.GraphNextArea(picCanvas);
+            tetris.InititializeData();
+        }
+
+        private void frmTetris_Load(object sender, EventArgs e)
+        {
+            tetris.InititializeData();
+        }
+
+        private void picPause_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void picPause_MouseEnter(object sender, EventArgs e)
+        {
+            tetris.GraphPause(picPause);
+        }
+
+        private void picPause_MouseLeave(object sender, EventArgs e)
+        {
+            tetris.RefreshPause(picPause);
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+    }
+}
